4a50653657141ae12bf73bf4a6992f4386101596
Unknown <jc_estevezr@hotmail.com>
2017-04-28 22:54:20 -0500

    Hospital Update

--- Hospital System/src/Data/Infrastructure/Hospital.java
+++ Hospital System/src/Data/Infrastructure/Hospital.java
@@ -8,6 +8,7 @@ package Data.Infrastructure;
 import Data.People.*;
 import Exceptions.NotValidNumber;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -67,38 +68,116 @@ public class Hospital {
         }else{
             this.address = "NOT DEFINED";
         }
-        
-        if(telephone != null || telephone.length() == 10){
-            this.telephone = telephone;
-        }else{
-            this.telephone = "NOT DEFINED";
+
+        try {
+            this.settelephone(telephone);
+        }catch(NotValidNumber error){
+            JOptionPane.showMessageDialog(null, error.toString(), "ERROR", JOptionPane.ERROR_MESSAGE);
         }
         
         try {
-            this.setdoctorID(doctorID);
+            this.setnumOfDoctorsOffices(numOfDoctorsOffices);
         }catch(NotValidNumber error){
             JOptionPane.showMessageDialog(null, error.toString(), "ERROR", JOptionPane.ERROR_MESSAGE);
-            this.doctorID = "NOT DEFINED";
         }
         
-        if()
-        this.numOfDoctorsOffices = numOfDoctorsOffices;
-        this.numOfRooms = numOfRooms;
+        try {
+            this.setnumOfRooms(numOfRooms);
+        }catch(NotValidNumber error){
+            JOptionPane.showMessageDialog(null, error.toString(), "ERROR", JOptionPane.ERROR_MESSAGE);
+        }
+        
         this.offices = new ArrayList<>();
         this.rooms = new ArrayList<>();
         this.doctors = new ArrayList<>();
         this.patients = new ArrayList<>();
     }
     
-    public void settelephone(String telephone) throws NotValidNumber{
-       
-        if(telephone != null || telephone.length() == 10){ 
-            this.telephone = telephone; 
-       }else {
-            throw new NotValidNumber("El numero de telefono no es valido");
+    public void setname(String name){
+        if(name != null){
+            this.name = name;
         }
     }
     
+    public void setaddress(String address){
+        if(address != null){
+            this.address = address;
+        }
+    }
+    
+    public void settelephone(String telephone) throws NotValidNumber{
+       
+        if(telephone != null && telephone.length() == 10){ 
+            this.telephone = telephone; 
+       }else {
+            this.telephone = "0000000000";
+            throw new NotValidNumber("El numero de telefono no es valido, telefono asignado por omision (000) 00 00 000");
+        }
+    }
+    
+    public void setnumOfDoctorsOffices(int numOfDoctorsOffices) throws NotValidNumber{
+       
+        if(numOfDoctorsOffices >= 0){ 
+            this.numOfDoctorsOffices = numOfDoctorsOffices; 
+       }else {
+            this.numOfDoctorsOffices = 0; 
+            throw new NotValidNumber("El numero de oficinas no es valido, numero de oficinas asignado por omision: 0");
+        }
+    }
+    
+    public void setnumOfRooms(int numOfRooms) throws NotValidNumber{
+       
+        if(numOfRooms >= 0){ 
+            this.numOfRooms = numOfRooms; 
+       }else {
+            this.numOfRooms = 0; 
+            throw new NotValidNumber("El numero de cuartos no es valido, numero de cuartos asignado por omision: 0");
+        }
+    }
+    
+    public String getname(){
+        return this.name;
+    }
+    
+    public String getaddress(){
+        return this.address;
+    }
+    
+    public int getnumOfDoctorsOffices(){
+        return this.numOfDoctorsOffices;
+    }
+    
+    public int getnumOfRooms(){
+        return this.numOfRooms;
+    }
+    
+    public ArrayList<DoctorsOffice> getoffices(){
+        return this.offices;
+    }
+    
+    public ArrayList<Room> getrooms(){
+        return this.rooms;
+    }
+    
+    public ArrayList<Doctor> getdoctors(){
+        return this.doctors;
+    }
+    
+    public ArrayList<Patient> getpatients(){
+        return this.patients;
+    }
+    
+    /*
+        private int id; // número entero que sirve de identificador de un consultorio dentro del hospital.
+    private int floor; //número entero que indica el piso en el que se encuentra el consultorio.
+    private float monthlyRent; // número real que indica cuánto cuesta mensualmente la renta del consultorio. 
+    private String doctorID; //identificador del médico al que está asignado el consultorio. 
+    private float lastPayment;
+    */
+    
+    public void addDoctorOffice(String id, ){
+        
+    }
     
     // Alan ------------------------------
     
