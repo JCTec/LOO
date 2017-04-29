6a4c1a0d6c7ca41387aec147e7d92e4aaf89cc14
oscarascal <oscar_hemelaar@hotmail.fr>
2017-04-28 19:02:51 -0500

    declared methods and variables + javadoc

--- Hospital System/src/Data/Infrastructure/Room.java
+++ Hospital System/src/Data/Infrastructure/Room.java
@@ -10,5 +10,46 @@ package Data.Infrastructure;
  * @author Juan
  */
 public class Room {
-    
+ 
+	/**
+	 * número entero que identifica al cuarto
+	 * van del 1 al número máximo de habitaciones definida por el hospital
+	 */
+	public int roomID;
+	
+	/**
+	 *	número de seguridad social del paciente asignado al cuarto
+	 */
+	public String patientID;
+
+	/**
+	 *
+	 * @param patientID
+	 * número de seguridad social del paciente asignado al cuarto
+	 */
+	public Room(String patientID) {
+		this.patientID = patientID;
+		//TODO make roomID incremental (how ? ask team)
+		this.roomID = 1; 
+	}
+	
+	/**
+	 *
+	 * @param room
+	 * objeto room para copiar
+	 */
+	public Room(Room room) {
+		this.roomID = room.roomID;
+		this.patientID = room.patientID;
+	}
+	
+	/**
+	 * patientID será "undefined patientID"
+	 */
+	public Room() {
+		this.patientID = "undefined patientID";
+		this.roomID = 1;
+	}
+	
+	
 }
