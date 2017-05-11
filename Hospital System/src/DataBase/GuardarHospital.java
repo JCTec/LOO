ac56545839e245c4616893e4d6173410ad72ba04
JC <jc_estevezr@hotmail.com>
2017-05-11 13:46:55 -0500

    GuardarHospital & LoadDataBase

---
+++ Hospital System/src/DataBase/GuardarHospital.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataBase;
+
+import Data.Infrastructure.Hospital;
+import com.thoughtworks.xstream.XStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Juan
+ */
+public class GuardarHospital {
+    private Hospital hospitalAGuardar;
+    private XStream xstream = new XStream();
+    
+    
+    public GuardarHospital(Hospital H){
+        this.hospitalAGuardar = H;
+        this.xstream.alias("Hospital", Hospital.class);
+        this.toXMl();
+    }
+    
+    public GuardarHospital(){
+        this.xstream.alias("Hospital", Hospital.class);
+    }
+    
+    private void toXMl(){
+        String xml = this.xstream.toXML(this.hospitalAGuardar);
+        this.Save(xml);
+    }
+    
+    public String toXMl(Hospital H){
+        return this.xstream.toXML(H);
+    }
+    
+    private void Save(String Xml){
+        
+        String name = System.getProperty("user.home") +  "/Documents/Hospitals/Hospital.txt";
+        String path = System.getProperty("user.home") +  "/Documents/Hospitals";
+        
+        File theDir = new File(path);
+        
+        if (!theDir.exists()) {
+            System.out.println("creating directory: " + theDir.getName());
+            boolean result = false;
+
+            try{
+              theDir.mkdir();
+                result = true;
+            } 
+            catch(SecurityException se){
+                
+            }        
+            if(result) {    
+                System.out.println("DIR created");  
+            }
+        }
+        
+        try(  PrintWriter out = new PrintWriter(name)  ){
+            out.println(Xml);
+            out.close();
+            
+        } catch (FileNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "Archivo no encontrado...", "ERROR", JOptionPane.ERROR_MESSAGE);
+        }
+        
+    }
+    
+    public void saveHospital(Hospital H, String Xml){
+         
+        String name = System.getProperty("user.home") +  "/Documents/Hospitals/Hospital.txt";
+        String path = System.getProperty("user.home") +  "/Documents/Hospitals";
+        
+        File theDir = new File(path);
+        
+        if (!theDir.exists()) {
+            System.out.println("creating directory: " + theDir.getName());
+            boolean result = false;
+
+            try{
+              theDir.mkdir();
+                result = true;
+            } 
+            catch(SecurityException se){
+                
+            }        
+            if(result) {    
+                System.out.println("DIR created");  
+            }
+        }
+        
+        try(  PrintWriter out = new PrintWriter(name)  ){
+            out.println(Xml);
+            out.close();
+            
+        } catch (FileNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "Archivo no encontrado...", "ERROR", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+    
+    
+}
---
+++ Hospital System/src/DataBase/LoadDataBase.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataBase;
+
+import Data.Infrastructure.Hospital;
+import java.io.IOException;
+import com.thoughtworks.xstream.XStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+/**
+ *
+ * @author Juan
+ */
+public class LoadDataBase {
+    private Hospital hospital;
+    private XStream xstream = new XStream();
+    
+    public LoadDataBase(){
+        String Xml = this.Get();
+        
+        if(Xml != "NOT_FOUND"){
+            
+            this.hospital = (Hospital)this.xstream.fromXML(Xml);
+            
+        }
+    }
+    
+    public Hospital LoadHospital(){
+        return this.hospital;
+    }
+    
+    private String Get(){
+        String name = System.getProperty("user.home") +  "/Documents/Hospitals/Hospital.txt";
+        
+        try {
+            return new String(Files.readAllBytes(Paths.get(name)));
+        } catch (IOException ex) {
+            return "NOT_FOUND";
+        }
+        
+    }
+}
